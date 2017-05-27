a303161090b61dedcf631b1ab866b56bff2261ab
meynard2109 <arielg2109@gmail.com>
2017-05-27 01:42:07 -0600

    Esdrey

--- Portatiles/Portatiles/Formularios/FrmPermisos.vb
+++ Portatiles/Portatiles/Formularios/FrmPermisos.vb
@@ -1,25 +1,7 @@
 ﻿Public Class FrmPermisos
     Dim i As Integer
     Private Sub Permisos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("simple error")
-
-        ' If 1 = 1 Then
-
-
-        MsgBox("Esta cosa nada tiene XD")
-
-
-        'mi codigo AADG
-        If True Then
-            MsgBox("Este es un fragmento de codigo hecho por AADG")
-            Dim i As Integer = 0
-            Do While (i < 5)
-                MsgBox("Este es el fragmento de codigo hecho por AADG")
-                i += 1
-            Loop
-        End If
-
-
+        MsgBox("Yo estoy cuadreando ESDREY")
 
     End Sub
 End Class
