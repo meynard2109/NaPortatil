4603aa151dbef0bf3ea128f21f8ccab08c3d1685
meynard2109 <arielg2109@gmail.com>
2017-05-27 00:48:34 -0600

    AHHHHHHHHHHHH

--- Portatiles/Portatiles/Formularios/FrmPermisos.vb
+++ Portatiles/Portatiles/Formularios/FrmPermisos.vb
@@ -2,5 +2,11 @@
 
     Private Sub Permisos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MsgBox("Esta cosa nada tiene XD")
+        MsgBox("Esta cosa nada tiene XD")
+        MsgBox("Esta cosa nada tiene XD")
+        MsgBox("Esta cosa nada tiene XD")
+
+        MsgBox("Esta cosa nada tiene XD")
+        MsgBox("Esta cosa nada tiene XD")
     End Sub
 End Class
