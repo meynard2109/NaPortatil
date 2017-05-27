c2c85c8c318c1af4397404d42bed7878c2e5ea15
arieldavg <aridg_rm87@yahoo.es>
2017-05-27 01:29:34 -0600

    yo hice cambiossa
    asdfasdfadsf

--- Portatiles/Portatiles/Formularios/FrmPermisos.vb
+++ Portatiles/Portatiles/Formularios/FrmPermisos.vb
@@ -7,5 +7,16 @@
         MsgBox("Esta cosa nada tiene XD")
 
 
+        'mi codigo AADG
+        If True Then
+            MsgBox("Este es un fragmento de codigo hecho por AADG")
+            Dim i As Integer = 0
+            Do While (i < 5)
+                MsgBox("Este es el fragmento de codigo hecho por AADG")
+                i += 1;
+            Loop
+        End If
+
+
     End Sub
 End Class
