1192d02a64c68ea7f61ee16d4139aec9a02d4138
meynard2109 <arielg2109@gmail.com>
2017-05-27 01:29:27 -0600

    Prueba Esdrey Multiple

--- Portatiles/Portatiles/Formularios/FrmPermisos.vb
+++ Portatiles/Portatiles/Formularios/FrmPermisos.vb
@@ -1,12 +1,15 @@
 ﻿Public Class FrmPermisos
-
+    Dim i As Integer
     Private Sub Permisos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MsgBox("Esta cosa nada tiene XD")
         MsgBox("Esta cosa nada tiene XD")
         MsgBox("Esta cosa nada tiene XD")
         MsgBox("Esta cosa nada tiene XD")
+        If 1 = 1 Then
 
-        MsgBox("Esta cosa nada tiene XD")
-        MsgBox("Esta cosa nada tiene XD")
+        End If
+        For Each i then
+
+        Next
     End Sub
 End Class
