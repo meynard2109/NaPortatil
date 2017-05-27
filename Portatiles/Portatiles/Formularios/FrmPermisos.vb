c75a0a180c43a5591702c8451016f4fce83aefde
meynard2109 <arielg2109@gmail.com>
2017-05-27 01:48:48 -0600

    otro

--- Portatiles/Portatiles/Formularios/FrmPermisos.vb
+++ Portatiles/Portatiles/Formularios/FrmPermisos.vb
@@ -1,7 +1,7 @@
 ﻿Public Class FrmPermisos
     Dim i As Integer
     Private Sub Permisos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Yo estoy cuadreando ESDREY")
+        MsgBox("Yo estoy cuadreando ESDREY") '''''''
 
     End Sub
 End Class
